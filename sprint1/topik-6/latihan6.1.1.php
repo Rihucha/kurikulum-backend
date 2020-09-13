114d00a82981fee0283f0639b6a4e7c174a754cf
Rihucha <70372598+Rihucha@users.noreply.github.com>
2020-09-13 21:28:03 +0700

    Add files via upload

---
+++ sprint1/topik-6/latihan6.1.1.php
@@ -0,0 +1,19 @@
+
+<?php
+    $calcRectangleArea = function($width, $length): int{
+        return $width*$length;
+    };
+
+    echo "=======================================\n";
+    echo "Program Menghitung Luas Persegi Panjang\n";
+    echo "=======================================\n";
+
+    echo "Inputkan nilai lebar : ";
+    $lebar = trim(fgets(STDIN));
+    echo "Inputkan nilai panjang : ";
+    $panjang = trim(fgets(STDIN));
+
+    $hasil = $calcRectangleArea($lebar,$panjang);
+
+    echo "Luas persegi panjang adalah ".$hasil."\n";
+?>
