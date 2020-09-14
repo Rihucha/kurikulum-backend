62f6a190203c6deffd0f700fec67dc043c9bff91
Rihucha <70372598+Rihucha@users.noreply.github.com>
2020-09-14 15:01:04 +0700

    Add files via upload

---
+++ sprint1/evaluasi1/evaluasi-1.php
@@ -0,0 +1,51 @@
+<?php
+// for ($i=1; $i <=9 ; $i++) { 
+//     for ($j=1; $j <=9 ; $j++) { 
+//         if ($j == 1 ||$j == 3 ||$j == 5 ||$j == 7 ||$j == 9){ 
+//             echo "+&nbsp";}
+//         else {
+//             echo "-&nbsp&nbsp";
+//         }}
+//         echo "<br>";
+// }
+// echo "<br>";
+// for ($i=1; $i <=9 ; $i++) { 
+//     for ($j=1; $j <=9 ; $j++) { 
+//         if ($i == 1 ||$i == 3 ||$i == 5 ||$i == 7 ||$i == 9){ 
+//             echo "+&nbsp";}
+//         else {
+//             echo "-&nbsp&nbsp";
+//         }
+//         echo "<br>";
+// }
+
+// echo "======================================== \n";
+// echo "| Selamat Datang Di Program Input Data | \n";
+// echo "======================================== \n";
+
+// echo "Data yang ingin diinputkan:";
+// $data =(int)(fgets(STDIN));
+// for ($i=1; $i <=$data ; $i++) { 
+//     echo "nama    : ";
+//     $datanama =(int)(fgets(STDIN));
+//     echo "NIK     : ";
+//     $datanik =(int)(fgets((STDIN));
+//     echo "jurusan : ";
+//     $datajurusan =(int)(fgets(STDIN));
+//     echo "divisi  : ";
+//     $datadivisi =(int)(fgets(STDIN));
+//     echo "Usia    : ";
+//     $datausia =(int)(fgets(STDIN));
+//     echo "\n";
+
+}
+function str_split_unicode($str, $l = 0) {
+    if ($l > 0){
+    $str ="5956560159466056";
+
+    $arr1 = str_split($str);
+    $arr2 = str_split($str,3)
+    }
+    print_r($arr1);
+    print_r($arr2);
+    
