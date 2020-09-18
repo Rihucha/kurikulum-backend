f090865cc406819ee0bcc2b04720d68c8cca28c7
Rihucha <70372598+Rihucha@users.noreply.github.com>
2020-09-18 14:59:03 +0700

    Add files via upload

---
+++ sprint2/topik1/evaluasi2/evaluasi.php
@@ -0,0 +1,37 @@
+<?php 
+
+class register{
+    public $santri = [
+        [
+          'name' => "Arief",
+          'nik'  => "OO1"
+        ]
+      ];
+      public static function insertStudent(array $santri): void{
+          return ($tambah_santri);
+    if  ($santri == array_push){
+        $tambah_santri = $self::$santri
+        // return = trim(fgets((STDIN)));
+            
+    }
+    Register::insertStudent(['name' => $name, 'nik' => $nik])
+        register::santri();
+        $isbn = trim(fgets(STDIN));
+        $nama = trim(fgets(STDIN));
+        
+    array_push($santri,"trim(fgets(STDIN)))")
+
+    // $santri = trim(fgets(STDIN));
+    // if($santri == "1"){
+                //  echo "santri yg mau di tambah kan"     
+                
+                // echo "Luas Persegi : ".$panjang*$tinggi." meter\n";
+                // echo "Keliling Persegi : ".(($panjang+$tinggi)*2)." meter\n";
+                
+                
+                // echo "Silahkan masukkan nama!\n";
+                // echo "Silahkan masukkan isbn!\n";
+
+
+
+}
