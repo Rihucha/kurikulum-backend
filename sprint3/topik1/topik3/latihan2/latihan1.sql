27232c80c14e7a8a2486367ce31613f29e023d18
Rihucha <70372598+Rihucha@users.noreply.github.com>
2020-09-25 10:10:29 +0700

    Add files via upload

---
+++ sprint3/topik1/topik3/latihan2/latihan1.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: latihan
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_namasantri` int(11) DEFAULT NULL,
+  `id_jurusan` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_namasantri` (`id_namasantri`),
+  KEY `id_jurusan` (`id_jurusan`),
+  CONSTRAINT `data_ibfk_1` FOREIGN KEY (`id_namasantri`) REFERENCES `santri` (`id`),
+  CONSTRAINT `data_ibfk_2` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data`
+--
+
+LOCK TABLES `data` WRITE;
+/*!40000 ALTER TABLE `data` DISABLE KEYS */;
+INSERT INTO `data` VALUES (1,1,1),(2,2,1),(3,3,2),(4,4,1);
+/*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jurusan`
+--
+
+DROP TABLE IF EXISTS `jurusan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jurusan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jurusan` varchar(255) NOT NULL,
+  `id_nama` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_nama` (`id_nama`),
+  CONSTRAINT `jurusan_ibfk_1` FOREIGN KEY (`id_nama`) REFERENCES `santri` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jurusan`
+--
+
+LOCK TABLES `jurusan` WRITE;
+/*!40000 ALTER TABLE `jurusan` DISABLE KEYS */;
+INSERT INTO `jurusan` VALUES (1,'listrik',NULL),(2,'audiovideo',NULL);
+/*!40000 ALTER TABLE `jurusan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `santri`
+--
+
+DROP TABLE IF EXISTS `santri`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `santri` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `namasantri` varchar(255) NOT NULL,
+  `id_jurusan` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_jurusan` (`id_jurusan`),
+  CONSTRAINT `santri_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `santri`
+--
+
+LOCK TABLES `santri` WRITE;
+/*!40000 ALTER TABLE `santri` DISABLE KEYS */;
+INSERT INTO `santri` VALUES (1,'muhammad',1),(2,'ridho',1),(3,'hushain',2),(4,'chan',1);
+/*!40000 ALTER TABLE `santri` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-25 10:03:37
